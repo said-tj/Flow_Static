5306fff4bafb5b1fb9baee8c9962a613c7381fa0
said-tj <said.tecpa.juarez.04@gmail.com>
2026-09-02 12:28:58 -0600

    Create abstract interface

---
+++ main.py
@@ -0,0 +1,7 @@
+print("|--------------------| \n")
+print("|        Menú        | \n")
+print("|--------------------|")
+
+# ¿Qué es aferente y eferente?
+#Aferente y eferente son términos utilizados en neurociencia para describir la dirección
+
